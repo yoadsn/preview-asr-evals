df0c4d8cb0359745bc1c4f58b3797a9166e4aa8c
Yoad <1684709+yoadsn@users.noreply.github.com>
2025-08-12 09:06:53 +0300

    Logout and user name on header

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,15 @@
+'use server';
+import { withAuth, signOut } from '@workos-inc/authkit-nextjs';
 import Link from 'next/link';
 
-export default function Header() {
+export default async function Header() {
+    const { user } = await withAuth({ ensureSignedIn: true });
+
+    async function signOutAction() {
+        'use server';
+        await signOut();
+    }
+
     return (
         <header className="bg-white shadow-sm">
             <nav className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -26,6 +35,14 @@ export default function Header() {
                         >
                             Transcribe Audio
                         </Link>
+                    </div>
+                    <div className="flex space-x-4 text-sm font-medium text-gray-700 text-center py-2">
+                        <form
+                            action={signOutAction}
+                        >
+                            <p>Welcome back {user?.firstName && `, ${user?.firstName}`}</p>
+                            <button className="underline" type="submit">Sign out</button>
+                        </form>
                     </div>
                 </div>
             </nav>
